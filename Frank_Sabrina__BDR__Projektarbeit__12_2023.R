2bb2f0862ebe364e389e0418f16bacfc57dafab4
Sabrina Frank <frank.sabrina.95@gmail.com>
2024-01-15 10:26:01 +0100

    Dataset upload

--- Frank_Sabrina__BDR__Projektarbeit__12_2023.R
+++ Frank_Sabrina__BDR__Projektarbeit__12_2023.R
@@ -2,3 +2,20 @@
 #git commit -m "adding a new file" 
 #git status
 #git push
+
+# Author: Sabrina Frank
+# Date: 15.01.2024
+# Project: Big Data Management
+
+####################################
+# Lung Cancer Risk Prediction in R #
+####################################
+
+# Installieren von benötigten Bibliotheken
+install.packages("data.table")       
+library("data.table")
+
+
+
+
+
