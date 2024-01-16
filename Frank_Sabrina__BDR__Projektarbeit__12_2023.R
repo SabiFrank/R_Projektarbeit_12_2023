47d1153dc797cfba4dfc1b5acbdf4aef914db154
Sabrina Frank <frank.sabrina.95@gmail.com>
2024-01-16 09:10:50 +0100

    Factorization

--- Frank_Sabrina__BDR__Projektarbeit__12_2023.R
+++ Frank_Sabrina__BDR__Projektarbeit__12_2023.R
@@ -14,12 +14,15 @@
 ##########################################
 # Installieren von benötigten Bibliotheken
 
+
 # install.packages("data.table")
 library("data.table")
 
 
-#########################################
+
+##########################################
 # Importieren und Sichten des Datensatzes
+
 
 ## Import mit fread
 lungcancer_raw <- fread(file = "./data/lung_cancer_patient_datasets.csv")
@@ -32,26 +35,36 @@ str(lungcancer_raw)
 summary <- summary(lungcancer_raw)
 
 
-#########################
+
+##########################################
 # Säubern des Datensatzes
 
+
 ## Patient ID und Index entfernen
-lungcancer_raw [,"Patient Id":=NULL]
-# ToDo: subsetting oder slicing schauen wue funktioniert
-lungcancer_raw
+lungcancer_raw[,(names(lungcancer_raw)[0:2]):=NULL]
 
 ## Gender umcodieren 1=Male, 2=Female
 lungcancer_raw[, Gender := as.character(Gender)][Gender == "1", Gender := "M"]
 lungcancer_raw[, Gender := as.character(Gender)][Gender == "2", Gender := "F"]
-lungcancer_raw
 
 ## Faktoren für Level einführen 
 ### https://datatables.net/forums/discussion/7001/solved-row-count-start-with-0-or-1
-lungcancer_raw[, "Air Pollution":=as.character("Air Pollution")]
-lungcancer_raw[, "Air Pollution":=as.character("Air Pollution")]
+lungcancer_raw[,3:24] <- lapply(lungcancer_raw[,3:24],as.factor)
+lungcancer_raw[,3:23] <- lapply(lungcancer_raw[,3:23],ordered)
 
+### Faktorordnung der Level-Spalte
+lungcancer_raw[, ("Level") := ordered(get("Level"), levels = c("Low", "Medium", "High"))]
+levels(lungcancer_raw$Level)
 str(lungcancer_raw)
 
+
+
+
+
+
+
+## ToDO vielleicht verwerfen
 ## Piping
-lungcancer <- lungcancer_raw |>
+# lungcancer <- lungcancer_raw |>
+                    
               
