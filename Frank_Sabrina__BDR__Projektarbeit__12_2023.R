8588a09e5a4a717eab6e55ac4b75e8c03fb33780
Sabrina Frank <frank.sabrina.95@gmail.com>
2024-01-17 12:00:09 +0100

    Updated ML Section

--- Frank_Sabrina__BDR__Projektarbeit__12_2023.R
+++ Frank_Sabrina__BDR__Projektarbeit__12_2023.R
@@ -12,7 +12,12 @@
 
 # install.packages("data.table")
 library("data.table")
-
+# install.packages("GGally")
+library(GGally) #Visualisierung Pairplots
+# install.packages("caret")
+library(caret) #ToDo wofür brauch ich das?
+# install.packages(utiml)
+library(utiml) #paper zitieren
 
 
 ##########################################
@@ -50,7 +55,7 @@ lungcancer_raw[, Gender := as.character(Gender)][Gender == "2", Gender := "F"]
 
 ## Faktoren für Level einführen 
 ### https://datatables.net/forums/discussion/7001/solved-row-count-start-with-0-or-1
-lungcancer_raw[,3:24] <- lapply(lungcancer_raw[,3:24],as.factor)
+lungcancer_raw[,2:24] <- lapply(lungcancer_raw[,3:24],as.factor) #ToDo: schauen, ob mit Gender als Faktor geht
 lungcancer_raw[,3:23] <- lapply(lungcancer_raw[,3:23],ordered)
 
 ### Faktorordnung der Level-Spalte
@@ -59,23 +64,77 @@ levels(lungcancer_raw$Level)
 str(lungcancer_raw)
 
 
+### in lungcancer_dt abspeichern
 
 
 
 ##########################################
-# Reduction (Korrelation suchen um vllt. Features zu entfernen)
+# Visualisierung des Datensatzes
 
+
+ggpairs(data) # braucht evtl. df statt dt
+
+lungcancer_dt %>% gather() %>%
+    ggplot(aes(x=value)) +
+    geom_histogram(fill="steelblue", alpha=0.7) +
+    theme_minimal() +
+    facet_wrap(~keyscales="free")
+
+# boxplot()
+# hist()
+
+#check balance of data
+
+
+
+##########################################
+# Reduction (Korrelation suchen um vllt. Features zu entfernen/ Feature Selection)
+# wenn corr größer 0.8 -> weg damit
+# https://www.r-bloggers.com/2022/02/beginners-guide-to-machine-learning-in-r-with-step-by-step-tutorial/
+
+lungcancer_dt %>% mutate_if(is.factor,as.numeric) 
+    %>% cor() 
+    %>% as.data.frame()
+    %>% select('Level')
+    %>% arrange(-Level)
+
+##barplots für categorische variablen
+
+# ToDo: X,y aufspalten??
+set.seed(1) #Todo checken??
+lungcancer_target <- lungcancer_dt$Level
+lungcancer_features <- lungcancer_dt |> .[0:23] |> 
 
 
 ##########################################
 # splitten des Datensatzes in Training und Test
 
+set.seed(1)
+split <- sample(nrow(lungcancer_dt),nrow(lungcancer_dt)*0.8) #ToDo schauen, ob 80/20 gut ist und vllt schreibweise aus corrlink
+train <- lungcancer_dt[split,-c(1:2)] #todo c vielleicht wegnehmen
+test <- lungcancer_dt[-split,-c(1:2)] #todo c vielleicht wegnehmen
+dim(train)
+dim(test)
+
+# mit utiml
+set.seed(123)
+ds <- create_holdout_partition(new.toyml, c(train=0.7, test=0.3), method="stratified")
+model <- br(ds$train, "RF")
+
 
 
 ##########################################
-# Encoden und Labeln
+# Encoden und Labeln, preprocessing
 
+### Normalisierung der Spalte Age
+train <- normalize_mldata(mdata) #Frage: vor split? oder nacher? 
 
+# k-folf stratified
+
+## Label Encoding und so
+
+## check balance of data
+# remove_skewness_labels(mdata) #evtl. checken, ob ich das brauch
 
 
 
@@ -83,13 +142,38 @@ str(lungcancer_raw)
 # Machine Learning
 # https://www.kaggle.com/code/takkimsncn/lung-cancer-prediction/notebook
 
+# Faktorisierung klappt vielleicht nicht mit ML model
+
+# which model to use: https://de.mathworks.com/campaigns/offers/next/choosing-the-best-machine-learning-classification-model-and-avoiding-overfitting.html
+# erst mal naive bayes probieren
+# bagged decision tree
+# svm gut, aber vllt. rechenintensiv, braucht viel tuning
+
+# k-fold cross val?
+
+measures <- c("macro-F1", "micro-F1")
+algorithm <- "SVM"
 
 
+###################################
+# Prediction and Confusionmatrix
+set.seed(12)
+
+prediction <- predict(MLmodel,newdata=test[-24])
+confusionMatrix(prediction,test$Level)
+
+# mit utiml
+predictions <- predict(model, ds$test)
+head(predictions)
+results <- multilabel_evaluate(ds$test, predictions, c("example-based", "macro-F1"))
+round(results, 4)
+
+cm <- multilabel_confusion_matrix(ds$test, predictions)
 
 
+## ToDO vielleicht machen
+### Piping https://www.r-bloggers.com/2021/05/the-new-r-pipe/
+### lungcancer <- lungcancer_raw |>
 
-## ToDO vielleicht verwerfen
-## Piping https://www.r-bloggers.com/2021/05/the-new-r-pipe/
-# lungcancer <- lungcancer_raw |>
-                    
-              
+### R Dashboard
+### https://www.r-bloggers.com/2021/11/top-r-packages-for-visualizing-table-data-make-stunning-tables-in-minutes/
